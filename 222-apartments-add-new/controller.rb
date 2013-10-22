c418864c4a8e3d96e28d42163f7247cec346a279
Daniel Stutzman <dtstutz@gmail.com>
2013-10-22 10:45:36 -0600

    Finished 222.

--- 222-apartments-add-new/controller.rb
+++ 222-apartments-add-new/controller.rb
@@ -1,5 +1,16 @@
 require './davinci-sinatra.rb'
 
 get "/" do
+  @apartments = Apartment.all
   halt erb(:index)
+end
+
+post "/" do
+  @apartment                 = Apartment.new
+  @apartment.address         = params["address"]
+  @apartment.rent_per_month  = params["rent_per_month"]
+  @apartment.does_allow_pets = params["does_allow_pets"]
+  @apartment.url             = params["url"]
+  @apartment.save!
+  redirect "/"
 end
--- 222-apartments-add-new/db/schema.rb
+++ 222-apartments-add-new/db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130724042221) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "apartments", force: true do |t|
+    t.string  "address"
+    t.integer "rent_per_month"
+    t.integer "num_bedrooms"
+    t.boolean "does_allow_pets"
+    t.string  "url"
+  end
 
 end
