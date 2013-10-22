8cbda4d6bdeef514f356a2e023e493665512095e
Daniel Stutzman <dtstutz@gmail.com>
2013-10-22 11:05:36 -0600

    Finished 219.

--- 219-post-requests/controller.rb
+++ 219-post-requests/controller.rb
@@ -3,3 +3,11 @@ require './davinci-sinatra.rb'
 get "/" do
   halt erb(:index)
 end
+
+# TODO: add a POST handler for the "/" route
+
+get "/elements/:id" do
+  id = params[:id]
+  @element = Element.find(id)
+  halt erb(:show)
+end
