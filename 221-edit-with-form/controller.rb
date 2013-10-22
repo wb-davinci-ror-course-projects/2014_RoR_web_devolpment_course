dfea69814ae3d784e194ab505f7d31e242ded45b
Daniel Stutzman <dtstutz@gmail.com>
2013-10-22 10:42:20 -0600

    Finished 221.

--- 221-edit-with-form/controller.rb
+++ 221-edit-with-form/controller.rb
@@ -1,5 +1,14 @@
 require './davinci-sinatra.rb'
 
 get "/" do
+  @patients = Patient.order(:id)
   halt erb(:index)
 end
+
+get "/patients/:id" do
+  id       = params["id"]
+  @patient = Patient.find(id)
+  halt erb(:edit)
+end
+
+# TODO: write POST handler
