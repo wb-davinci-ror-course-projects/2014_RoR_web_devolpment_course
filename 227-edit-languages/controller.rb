6a9689c060f84e16ebf1cc4e11ba2638539d31df
Daniel Stutzman <dtstutz@gmail.com>
2013-10-24 21:51:52 -0600

    Now you can add a new language too.

--- 227-edit-languages/controller.rb
+++ 227-edit-languages/controller.rb
@@ -5,10 +5,17 @@ get "/" do
   halt erb(:index)
 end
 
+get "/new_language" do
+  @language = Language.new
+  halt erb(:edit)
+end
+
+# TODO: write a POST handler to create a language
+
 get "/languages/:id" do
   id         = params["id"]
   @language  = Language.find(id)
   halt erb(:edit)
 end
 
-# TODO: write a POST handler
+# TODO: write a POST handler to update a language
