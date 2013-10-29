b7f69d45fbb9eb51c8a0630fb26c3eeb4ef6bf62
Daniel Stutzman <dtstutz@gmail.com>
2013-10-29 10:56:08 -0600

    Finished 234-name-in-route.

--- 234-name-in-route/controller.rb
+++ 234-name-in-route/controller.rb
@@ -1,5 +1,10 @@
 require './davinci-sinatra.rb'
 
 get "/" do
+  @entries = EncyclopediaEntry.order(:word)
   halt erb(:index)
 end
+
+# TODO: write GET handler to serve show.html.erb with @entry set
+# to the correct Encyclopedia Entry for routes like /albatross 
+# and /jay
