ee9668fb4d0ac906bf42080fc9d380e16f6e4e62
Daniel Stutzman <dtstutz@gmail.com>
2013-11-05 12:54:21 -0700

    Added 246-player-belongs-to as duplicate of 243.

---
+++ 246-player-belongs-to/db/seeds.rb
@@ -0,0 +1,7 @@
+Team.create! id: 1, name: "Bears"
+Team.create! id: 2, name: "Tigers"
+Team.create! id: 3, name: "Lions"
+Player.create! name: "Jill", team_id: 1
+Player.create! name: "Ted",  team_id: 1
+Player.create! name: "Lena", team_id: 2
+Player.create! name: "Jack", team_id: 3
