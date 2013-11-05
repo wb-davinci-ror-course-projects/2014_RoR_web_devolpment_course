31a201054b3dee219c76c2df853048002f724e42
Daniel Stutzman <dtstutz@gmail.com>
2013-11-05 12:11:22 -0700

    Finished 244-cars.

--- 244-cars/db/schema.rb
+++ 244-cars/db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131105170921) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cars", force: true do |t|
+    t.integer "member_id"
+    t.string  "model"
+  end
+
+  create_table "members", force: true do |t|
+    t.string "name"
+  end
 
 end
--- 244-cars/db/seeds.rb
+++ 244-cars/db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Member.create! id: 1, name: "Dale"
+
+Car.create! model: "Jetta", member_id: 1
+
+
+
+Member.create! id: 2, name: "Fran"
+
+Car.create! model: "Element", member_id: 2
+
+Car.create! model: "F-150", member_id: 2
+
+
+
+Member.create! id: 3, name: "Ella"
+
+
+
+Car.create! model: "Beetle", member_id: nil
