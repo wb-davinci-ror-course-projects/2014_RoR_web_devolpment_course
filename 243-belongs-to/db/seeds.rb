ea267a953bd56ae519f182b717eb7f76f5910bb1
Daniel Stutzman <dtstutz@gmail.com>
2013-11-05 11:50:20 -0700

    Finished 243-belongs-to.

--- 243-belongs-to/db/seeds.rb
+++ 243-belongs-to/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Team.create! id: 1, name: "Bears"
+Team.create! id: 2, name: "Tigers"
+Team.create! id: 3, name: "Lions"
+Player.create! name: "Jill", team_id: 1
+Player.create! name: "Ted",  team_id: 1
+Player.create! name: "Lena", team_id: 2
+Player.create! name: "Jack", team_id: 3
