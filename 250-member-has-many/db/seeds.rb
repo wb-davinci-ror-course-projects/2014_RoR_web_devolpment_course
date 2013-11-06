c429e4ab390c45d0f52f8d166fd3b9bbbc656788
Daniel Stutzman <dtstutz@gmail.com>
2013-11-05 22:44:46 -0700

    Added 250-member-has-many.

---
+++ 250-member-has-many/db/seeds.rb
@@ -0,0 +1,19 @@
+Member.create! id: 1, name: "Dale"
+
+Car.create! model: "Jetta", member_id: 1
+
+
+
+Member.create! id: 2, name: "Fran"
+
+Car.create! model: "Element", member_id: 2
+
+Car.create! model: "F-150", member_id: 2
+
+
+
+Member.create! id: 3, name: "Ella"
+
+
+
+Car.create! model: "Beetle", member_id: nil
