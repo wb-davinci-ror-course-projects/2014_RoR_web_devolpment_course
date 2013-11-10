fe0b2e733ec157aa872cb07ff989388dcb0f821f
Daniel Stutzman <dtstutz@gmail.com>
2013-11-10 14:49:17 -0700

    Finished 254-prefs-in-session.

--- 254-prefs-in-session/controller.rb
+++ 254-prefs-in-session/controller.rb
@@ -1,5 +1,18 @@
 require './davinci-sinatra.rb'
 
 get "/" do
+  # TODO: Retrieve three values stored in the session and use them
+  # to pre-fill form values in the view.
+
   halt erb(:index)
 end
+
+post "/" do
+  # TODO: Transfer values of POST params to become local variables.
+  # TODO: Save those local variables to the session so the values
+  #       will last.
+
+  redirect "/"
+end
+
+# TODO: Write handler for GET /logout that empties the session
