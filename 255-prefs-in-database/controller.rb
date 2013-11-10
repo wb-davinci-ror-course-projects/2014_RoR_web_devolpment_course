a0d120c6b143a1db678329829865d3514a2d98a7
Daniel Stutzman <dtstutz@gmail.com>
2013-11-10 15:10:58 -0700

    Finished 255-prefs-in-database.

--- 255-prefs-in-database/controller.rb
+++ 255-prefs-in-database/controller.rb
@@ -1,5 +1,26 @@
 require './davinci-sinatra.rb'
 
 get "/" do
+  # TODO: If there is already a user_id saved in the session...
+  # TODO: ...then use it to load the @user instance variable.
+  # TODO: Otherwise just start a new empty User.
+
   halt erb(:index)
 end
+
+post "/" do
+  # TODO: If there is already a user_id saved in the session...
+  # TODO: ...then use it to load the @user instance variable.
+  # TODO: Otherwise just start a new empty User.
+
+  # TODO: Set the user's new preferences from POST params on the User
+  # TODO: Then save the User object
+
+  # TODO: Save that user ID to the session, in case we created a new
+  #       one. (If we already had a user ID in the session, it doesn't
+  #       hurt to overwrite it with the same value).
+
+  redirect "/"
+end
+
+# TODO: Write handler for GET /logout
--- 255-prefs-in-database/db/schema.rb
+++ 255-prefs-in-database/db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130817210832) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string "color"
+    t.string "number"
+    t.string "letter"
+  end
 
 end
