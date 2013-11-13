cde445e65983426513708aa4fbf839b932b17f1f
Daniel Stutzman <dtstutz@gmail.com>
2013-11-12 22:25:04 -0700

    Finished 256-has-secure-password.

--- 256-has-secure-password/controller.rb
+++ 256-has-secure-password/controller.rb
@@ -1,5 +1,37 @@
 require './davinci-sinatra.rb'
 
+# Users always expect a route at / so redirect them to where we want
 get "/" do
-  halt erb(:index)
+  redirect "/login"
 end
+
+# Setup GET route for registration form
+get "/users/new" do
+
+  # Define @user so new.html.erb view can use it to pre-fill in blanks
+  @user = User.new
+
+  halt erb(:new)
+end
+
+# TODO: Write POST route for handling registration form submissions
+
+# Setup GET route for login form
+get "/login" do
+  halt erb(:login)
+end
+
+# TODO: Write POST route to handle login form submissions
+
+# An example page that's accessible only after you're logged in
+get "/welcome" do
+  # Get the user_id out of the session
+  user_id = session[:user_id]
+
+  # Look up the user row with that id
+  @user = User.find(user_id)
+
+  halt erb(:welcome)
+end
+
+# TODO: Write handler for GET /logout route, to clear the session.
