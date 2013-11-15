ccc796067dbf9f02b532f838c4a4d0a8ba211ece
Daniel Stutzman <dtstutz@gmail.com>
2013-11-14 19:36:38 -0700

    Finished 259.

--- 259-flash-now-login/controller.rb
+++ 259-flash-now-login/controller.rb
@@ -1,5 +1,60 @@
 require './davinci-sinatra.rb'
 
 get "/" do
-  halt erb(:index)
+  redirect "/login"
+end
+
+get "/login" do
+  # There's no error to show; not necessary, just being clear...
+  @error = nil
+
+  halt erb(:login)
+end
+
+post "/login" do
+  # Load the values that user filled out for the username and password
+  # into variables.
+  username = params["username"]
+  password = params["password"]
+
+  # Look for a row in the users table with that same username
+  found_user = User.find_by(username: username)
+
+  # If no row was found for that username...
+  if found_user == nil
+    # Then set a message to show at the top of the page
+    @error = "Unknown username"
+
+    # And render the login page again
+    halt erb(:login)
+
+  # Otherwise, if the password was wrong...
+  elsif found_user.authenticate(password) == false
+    # Then set a message to show at the top of the page
+    @error = "Incorrect password"
+
+    # And render the login page again
+    halt erb(:login)
+
+  # Otherwise... (if the username and password was right)
+  else
+    # Save that user's id into the session so we'll have it later
+    session[:user_id] = found_user.id
+
+    # Redirect to the default page
+    redirect "/welcome"
+  end # End if
+end # End handler
+
+get "/welcome" do
+  # Load the user_id from the session
+  user_id = session[:user_id]
+  # Load the user based on that id saved to the session
+  @user = User.find(user_id)
+  halt erb(:welcome)
+end
+
+get "/logout" do
+  session.clear
+  redirect "/login"
 end
--- 259-flash-now-login/db/seeds.rb
+++ 259-flash-now-login/db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! username: "abe", password: "123", password_confirmation: "123"
+User.create! username: "amy", password: "456", password_confirmation: "456"
