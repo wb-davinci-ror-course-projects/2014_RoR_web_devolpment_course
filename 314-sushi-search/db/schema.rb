615015771a0caca38367c0b639909da8937bf171
Daniel Stutzman <dtstutz@gmail.com>
2013-11-30 15:20:57 -0700

    Finished 314-sushi-search.

--- 314-sushi-search/db/schema.rb
+++ 314-sushi-search/db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131130221526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menu_items", force: true do |t|
+    t.string   "name"
+    t.float    "price"
+    t.string   "restaurant_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
