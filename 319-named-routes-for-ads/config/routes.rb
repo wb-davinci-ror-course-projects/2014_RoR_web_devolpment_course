0673ba4a508ab588278016de258f368f753a4d2b
Daniel Stutzman <dtstutz@gmail.com>
2013-12-03 13:45:27 -0700

    Finished 319.

--- 319-named-routes-for-ads/config/routes.rb
+++ 319-named-routes-for-ads/config/routes.rb
@@ -1,56 +1,15 @@
 App::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get  "/"               => "ads#root"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get  "/list_ads"       => "ads#list_ads"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get  "/new_ad"         => "ads#new_ad"
+  post "/new_ad"         => "ads#new_ad_post"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get  "/show_ad/:id"    => "ads#show_ad"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get  "/edit_ad/:id"    => "ads#edit_ad"
+  post "/edit_ad/:id"    => "ads#edit_ad_post"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get  "/destroy_ad/:id" => "ads#destroy_ad"
 end
