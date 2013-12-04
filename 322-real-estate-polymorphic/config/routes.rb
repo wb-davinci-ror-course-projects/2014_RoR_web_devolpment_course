1caf82b8a2868444ae69bf17e632d663f25bc1ef
Daniel Stutzman <dtstutz@gmail.com>
2013-12-03 18:42:02 -0700

    Added 322.

--- 322-real-estate-polymorphic/config/routes.rb
+++ 322-real-estate-polymorphic/config/routes.rb
@@ -1,56 +1,28 @@
 App::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get  "/" => "houses#root"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources "houses", path: "/houses", controller: "houses",
+    only: [:index, :show]
+  # The above resources lines expand to:
+  #   get  "/houses"     => "houses#index"
+  #   get  "/houses/:id" => "houses#show"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources "sessions", path: "/sessions", controller: "sessions",
+    only: [:new, :create, :destroy]
+  # The above resources lines expand to:
+  #   get    "/sessions/new" => "sessions#new"
+  #   post   "/sessions"     => "sessions#create"
+  #   delete "/sessions"     => "sessions#destroy"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources "admin_houses", path: "/admin_houses", controller: "admin_houses",
+    only: [:index, :new, :create, :edit, :update, :destroy]
+  # The above resources lines expand to:
+  #   get    "/admin_houses"          => "admin_houses#index"
+  #   get    "/admin_houses/new"      => "admin_houses#new"
+  #   post   "/admin_houses"          => "admin_houses#create"
+  #   get    "/admin_houses/:id/edit" => "admin_houses#edit"
+  #   put    "/admin_houses/:id"      => "admin_houses#update"
+  #   delete "/admin_houses/:id"      => "admin_houses#destroy"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
--- 322-real-estate-polymorphic/db/seeds.rb
+++ 322-real-estate-polymorphic/db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+House.create! \
+  address:         "123 Easy St",
+  city:            "Louisville",
+  state:           "CO",
+  price:           "$700,000",
+  num_bedrooms:    3,
+  num_stories:     2,
+  num_square_feet: "5,000",
+  image_url:       "http://farm1.staticflickr.com/55/139445633_e2fabef491.jpg"
+
+House.create! \
+  address:         "35 Oak Ln",
+  city:            "Lafayette",
+  state:           "CO",
+  price:           "$300,000",
+  num_bedrooms:    3,
+  num_stories:     2,
+  num_square_feet: "5,000",
+  image_url:       "http://farm1.staticflickr.com/104/292132478_05c200a320.jpg"
+
+House.create! \
+  address:         "254 Marigold Ct",
+  city:            "Boulder",
+  state:           "CO",
+  price:           "$900,000",
+  num_bedrooms:    3,
+  num_stories:     1,
+  num_square_feet: "7,000",
+  image_url:       "http://farm8.staticflickr.com/7047/6973026131_8e733ee374.jpg"
+
+Admin.create! username: "bob", password: "123", password_confirmation: "123"
+Admin.create! username: "sue", password: "234", password_confirmation: "234"
