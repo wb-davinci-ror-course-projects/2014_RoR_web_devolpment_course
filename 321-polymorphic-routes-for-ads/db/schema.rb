bb5278f718e567a53f6070dd78fd4d424441e8c9
Daniel Stutzman <dtstutz@gmail.com>
2013-12-03 18:34:38 -0700

    Finished 321.

--- 321-polymorphic-routes-for-ads/db/schema.rb
+++ 321-polymorphic-routes-for-ads/db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130907231558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ads", force: true do |t|
+    t.string   "company_name"
+    t.string   "duration"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
