36a1d09701237c94dd87ab3fc05e2ddc5997e674
Daniel Stutzman <dtstutz@gmail.com>
2013-12-07 17:30:47 -0700

    Added seeds to 327.

--- 327-dates/db/seeds.rb
+++ 327-dates/db/seeds.rb
@@ -1,1 +1,11 @@
-Event.create! name: "RailsConf 2014"
+Event.create! name: "RubyConf Australia 2014",
+  start_date: "2014-02-19",
+  end_date: "2014-02-21"
+
+Event.create! name: "RubyConf India 2014",
+  start_date: "2014-03-22",
+  end_date: "2014-03-23"
+
+Event.create! name: "RailsConf 2014",
+  start_date: "2014-04-22",
+  end_date: "2014-04-25"
