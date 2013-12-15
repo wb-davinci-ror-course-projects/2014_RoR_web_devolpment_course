c870305c848642530928cc03793e3c82223df6ef
Daniel Stutzman <dtstutz@gmail.com>
2013-12-15 15:43:11 -0700

    Finished 336.

---
+++ 336-model-methods/app/models/person.rb
@@ -0,0 +1,5 @@
+class Person < ActiveRecord::Base
+  validates :first_name,  presence: true
+  validates :last_name,   presence: true
+  validates :birth_date,  presence: true
+end
--- 336-model-methods/db/schema.rb
+++ 336-model-methods/db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130924173042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birth_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
