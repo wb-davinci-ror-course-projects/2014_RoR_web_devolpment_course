ec6a34a975424f3dc3f4bd5a2b8229ea6a7cd3ad
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-02-17 20:06:12 -0700

    218_hmw_added to controller and link to show_assignment complete

--- 218-rake-console/controller.rb
+++ 218-rake-console/controller.rb
@@ -15,3 +15,17 @@ get "/cities/:id" do
   @city = WorldCity.find(id)
   halt erb(:show_city)
 end
+
+get "/create_element" do
+Element.create! name: "Carbon"
+  halt erb(:index)
+end
+
+get "/add_population/" do
+  w = WorldCity.find(2)
+  w.population_in_millions= w.population_in_millions * 2
+  WorldCity.create! population_in_millions: w
+  w.save!
+  halt erb(:index)
+end
+
