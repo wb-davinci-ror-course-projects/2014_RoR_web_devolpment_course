2e21ab0b5577b066490d8dcaccb31443530017f7
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-02-22 19:40:20 -0700

    226_presidents_hmw_assignment complete

--- 226-edit-presidents/controller.rb
+++ 226-edit-presidents/controller.rb
@@ -18,4 +18,27 @@ get "/presidents/:id" do
   halt erb(:edit)
 end
 
-# TODO: write POST handler to update president
+post '/new_president' do
+  @president    = President.new
+  @president.name = params["name"]
+  @president.biography = params["biography"]
+  @president.ended_term_early =["ended_term"]
+  @president.party = params["party"]
+  @president.term_number = params["term_number"]
+  @president.birth_state_id = params["birth_state_id "]
+  @president.former_occupation = params["former_occupation"]
+  @president.save!
+  redirect "/"
+end
+
+post '/presidents/:id' do
+  id               = params["id"]
+  @president       = President.find(id)
+  @president.biography = params["biography"]
+  @president.ended_term_early = params["ended_term_early"]
+  @president.party = params["party"]
+  @president.former_occupation = params["former_occupation"]
+  @president.birth_state_id = params["birth_state_id"]
+  @president.save!
+  redirect "/"
+end
