877c872dcba761646030172ea5f6aa5ffea5bcfc
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-02-23 18:17:21 -0700

    220_contact_exercise_complete

--- 220-contact-us/controller.rb
+++ 220-contact-us/controller.rb
@@ -1,14 +1,14 @@
 require './davinci-sinatra.rb'
 
 get "/" do
+  @message = Message.all
   halt erb(:index)
 end
 
-post "/contact_us" do
+post "/" do
   message    = Message.new
   message.subject = params["subject"]
   message.body    =params["body"]
   message.save!
   redirect "/"
 end
-
