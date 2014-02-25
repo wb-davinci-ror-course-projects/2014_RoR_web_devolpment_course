7f744053013c8a868b2e6f841e5b13d356937af2
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-02-24 20:54:29 -0700

    229_shoes_page 1 and 2 of 4 done

--- 229-shoe-club/controller.rb
+++ 229-shoe-club/controller.rb
@@ -14,27 +14,43 @@ end
 
 post "/sign_up" do
   @customer = Customer.find(1)
-
-  # TODO: If Go Back was clicked, go back to the previous page
-  # TODO: If Continue was clicked, save the entered info and either go on
-  #       to next page, or show validation errors on this same page
+  @customer.first_name = params[:first_name]
+  @customer.last_name = params[:last_name]
+  if params[:commit] == "Continue"
+    if @customer.save == true
+      redirect "/shipping"
+    else
+      halt erb(:sign_up)
+    end
+  elsif params[:commit] == "Go back"
+    redirect "/sign_up"  
+  end
 end
 
 get "/shipping" do
   @customer = Customer.find(1)
   @u_s_states = USState.order(:name).all
-
+  
   halt erb(:shipping)
 end
 
 post "/shipping" do
   @customer = Customer.find(1)
   @u_s_states = USState.order(:name).all
-
-  # TODO: If Go Back was clicked, go back to the previous page
-  # TODO: If Continue was clicked, save the entered info and either go on
-  #       to next page, or show validation errors on this same page
-end
+  @customer.ship_address1   = params[:ship_address1]
+  @customer.ship_city = params[:ship_city]
+  @customer.ship_state = params[:ship_state]
+  @customer.ship_speed = params[:ship_speed]
+  @customer.ship_zip_code = params[:ship_zip_code]
+  if params[:commit] == "Continue"
+    if @customer.save == true
+      redirect "/billing"
+    else
+      halt erb(:shipping)
+    end
+  elsif params[:commit] == "Go back"
+    redirect "/shipping"  
+  end
 
 get "/billing" do
   @customer = Customer.find(1)
