e927fab2f130a9314850ce857e0377723ba6a205
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-01 14:44:53 -0700

    237_names_hmw_complete

--- 237-same-first-names/controller.rb
+++ 237-same-first-names/controller.rb
@@ -5,14 +5,23 @@ get "/" do
 end
 
 post "/" do
-  # TODO: If no one has that first name, show no_one.html.erb
-  # TODO: If one person has that first name, show number.html.erb with
-  #       their info.
-  # TODO: If 2+ people have that first name, show choose.html.erb
-  #       to let the user choose between the people with that first name.
-end
+  first_name = params["first_name"]
+  people = Person.where(first_name: first_name)
+  if people.size == 0
+    halt erb(:no_one)
+  elsif people.size >1
+    @people = people
+    halt erb(:choose)
+  else
+    @person = Person.find_by(first_name: first_name)
+      halt erb(:number)
+  end
+end 
 
 get "/person/:first/:last" do
-  # TODO: Show the info for the person with the first and last name
-  #       specified in the route.
+  first = params[:first]
+  last = params[:last]
+  @person = Person.find_by(first_name: first, last_name: last)
+  halt erb(:number)
 end
+
