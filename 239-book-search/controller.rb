a9c54908d4b8175ba49c9f595ecce4a232af1c1a
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-01 20:35:47 -0700

    239_book_hmw_complete

--- 239-book-search/controller.rb
+++ 239-book-search/controller.rb
@@ -1,14 +1,34 @@
 require './davinci-sinatra.rb'
 
 get "/" do
-  @books = Book.order(:id).all
   halt erb(:browse)
 end
 
-get "/author/:author" do
-  author = params[:author]
-  @book = Book.order(:author).where(author: author)
+get "/topic/:language" do
+  language = params[:language]
+  @books = Book.where("title ilike ?", "%#{language}%")
+  @header = "Books about #{language}"
   halt erb(:search_results)
+end
+
+get "/author/:last_name" do
+  last_name = params[:last_name]
+  @books = Book.where("author ilike ?", "%#{last_name}")
+  @header = "Books by #{last_name}"
+  halt erb(:search_results)
+end
+
+get "/year/:year" do
+  year = params[:year]
+  @books = Book.where(publication_year: year)
+  @header = "Books published in #{year}"
+  halt erb(:search_results)
+end
+
+get "/isbn/:number" do
+  number = params[:number]
+  @books = Book.where(isbn: number)
+  halt erb(:show)
 end
 
 # TODO: GET handler for routes like /year/2011, /year/2007, etc.
