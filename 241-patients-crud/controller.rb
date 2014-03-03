81fd3aa2971d26acdff46ca172c603b3a9623962
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-03 13:13:27 -0700

    241_patients_hmw_complete

--- 241-patients-crud/controller.rb
+++ 241-patients-crud/controller.rb
@@ -1,6 +1,55 @@
 require './davinci-sinatra.rb'
 
-# TODO: write handler for GET / requests
+get "/" do
+  @patients = Patient.all
+  halt erb(:index)
+end
+
+get "/patients/:id" do
+  id = params[:id]
+  @patient = Patient.find(id)
+  halt erb(:edit_or_new)
+end
+
+post "/patients/:id" do
+  id = params[:id]
+  
+  if params["commit"] == "Update" 
+  @patient = Patient.find(id)
+  @patient.systolic = params[:systolic]
+  @patient.diastolic = params[:diastolic]
+  
+  if @patient.save == true
+  redirect "/"
+  
+  else 
+    halt erb(:edit_or_new)
+end
+
+elsif params["commit"] == "Delete" 
+  @patient = Patient.find(id)
+  @patient.destroy
+  redirect "/"
+  end
+end
+
+get "/new_patient" do
+@patient = Patient.new
+halt erb(:edit_or_new)
+end
+
+post "/new_patient" do
+  @patient = Patient.new
+  @patient.systolic = params[:systolic]
+  @patient.diastolic = params[:diastolic]
+  
+  if @patient.save == true
+  redirect "/"
+  else 
+    halt erb(:edit_or_new)
+  end
+end
+
 
 # TODO: write handler for GET /patients/3, /patients/5, etc. requests
 
