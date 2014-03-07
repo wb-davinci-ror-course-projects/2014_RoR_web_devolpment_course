d25fcd6472b7bcd255de5672469c19688c05dde4
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-06 21:40:14 -0700

    252_employee_using with belongs_to method_complete

--- 252-employee-belongs-to/controller.rb
+++ 252-employee-belongs-to/controller.rb
@@ -1,5 +1,22 @@
 require './davinci-sinatra.rb'
 
-# TODO: Write handler for GET /
+get "/" do
+  @employees = Employee.order(:id)
+  @new_employee = Employee.new
+  halt erb(:index)
+end
 
-# TODO: Write handler for POST /
+post "/" do
+  @new_employee = Employee.new
+  @new_employee.first_name = params["first_name"]
+  @new_employee.last_name = params["last_name"]
+  @new_employee.company_id = params["name"]
+  
+  if @new_employee.save == true
+    redirect "/"
+      else
+        @employees = Employee.order(:id)
+        halt erb(:index)
+  end
+end
+
