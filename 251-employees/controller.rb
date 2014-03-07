5e30c77e683f95c319543af8e5ee448a09efdfc6
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-06 19:45:41 -0700

    251_employees_hmw_first mockup matched

--- 251-employees/controller.rb
+++ 251-employees/controller.rb
@@ -1,5 +1,22 @@
 require './davinci-sinatra.rb'
 
-# TODO: Write handler for GET /
+get '/' do
+  @employees = Employee.all
+  @new_employee = Employee.new
+  halt erb(:index)
+end
 
-# TODO: Write handler for POST /
+post "/" do
+  @new_employee = Employee.new # or Company.new
+  @new_employee.first_name = params["first_name"]
+  @new_employee.company_id = params ["company_id"]
+
+  if @new_employee.save == true
+  redirect "/"
+  
+  else
+   @employees = Employee.order(:id)
+  halt erb(:index)
+  end
+end
+
--- 251-employees/models/employee.rb
+++ 251-employees/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
+  belongs_to :company, class_name: "Company", foreign_key: "company_id"
 end
