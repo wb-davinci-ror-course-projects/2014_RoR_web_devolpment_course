028f97ffb79f011217c216777bb3be401d4928dc
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-07 20:01:17 -0700

    242_recipes_hmw_completed

--- 242-recipes/controller.rb
+++ 242-recipes/controller.rb
@@ -3,6 +3,7 @@ require './davinci-sinatra.rb'
 get "/" do
   @recipes = Recipe.order(:id).all
   @title = "All recipes"
+  @author = Author.order(:id).each
   halt erb(:index)
 end
 
@@ -10,6 +11,14 @@ get "/recipe/:name" do
   name = params["name"]
   @recipe = Recipe.find_by(name: name)
   halt erb(:show)
+end
+
+get "/by_author/:first_name" do
+  first_name = params["first_name"]
+  @by_author = Author.find_by(first_name: first_name)
+  @recipes = Recipe.order(:id)
+  @title = "Recipes by #{first_name}"
+  halt erb(:index)
 end
 
 # Add handler for routes like GET /by-author/Jim and GET /by-author/Jen
