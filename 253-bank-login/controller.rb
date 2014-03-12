706a693c10e8251c557b88c4b9d381b7966edf4a
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-11 19:51:50 -0600

    253_bank-login_class work_completed

--- 253-bank-login/controller.rb
+++ 253-bank-login/controller.rb
@@ -5,39 +5,34 @@ get "/" do
 end
 
 get "/login" do
-  # There's no error to show; not necessary, just being clear...
   @error = nil
-
   halt erb(:login)
 end
 
 post "/login" do
-  # TODO: Take the username and password that the user entered,
-  # and either log them in, saving their user ID to the session,
-  # and redirect to /accounts, or set an error message in @error
-  # and re-render login.html.erb.
-
-  redirect "/accounts"
-end
+  username = params["username"]
+  user = User.find_by(username: username)
+  if user == nil
+    @error = "Unknown username"
+    halt erb(:login)
+  else
+    session[:user_id] = user.id
+    redirect "accounts"
+    end
+  end
 
 get "/accounts" do
-  @user = User.find(1) # TODO: Remove this line
-  # TODO: Load the user based on the id saved to the session
-
+  @user = User.find_by(id: session[:user_id])
   halt erb(:accounts)
 end
 
 get "/location" do
-  @user = User.find(1) # TODO: Remove this line
-  # TODO: Load the user based on the id saved to the session
-
+  @user = User.find_by(id: session[:user_id])
   halt erb(:location)
 end
 
 get "/rates" do
-  @user = User.find(1) # TODO: Remove this line
-  # TODO: Load the user based on the id saved to the session
-
+  @user = User.find_by(id: session[:user_id])
   halt erb(:rates)
 end
 
