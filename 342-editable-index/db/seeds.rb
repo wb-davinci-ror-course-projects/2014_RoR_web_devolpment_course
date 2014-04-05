5434e403efaa4c2863cd674c1470c0ef0323185e
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-04-05 17:19:03 -0600

    ATSrails_setup

--- 342-editable-index/db/seeds.rb
+++ 342-editable-index/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Customer.create! fname: "Tina", lname: "Ortiz"
+Customer.create! fname: "Don", lname: "Durham"
