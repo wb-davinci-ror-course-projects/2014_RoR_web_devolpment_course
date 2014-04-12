57ad41ae2becc3c63f0b7131b1a40c929805d306
Daniel Stutzman <dtstutz@gmail.com>
2014-04-12 15:49:07 -0600

    Finished 343.

--- 343-add-pagination/db/seeds.rb
+++ 343-add-pagination/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..100).each do |i|
+  user = User.new
+  user.email = "user#{i}@example.com"
+  user.password = "123"
+  user.password_confirmation = "123"
+  user.save!
+end
